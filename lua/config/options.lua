feb50d2afd643ae185f8ed830f0781f8c0ac1184
刘天增 <tianzeng_liu@loongjoy.com>
2026-01-19 11:41:29 +0800

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,3 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://gith, "QuitPre", "VimSuspend"ub.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,22 @@
+return {
+  "okuuva/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" }, -- 可选：指定触发加载的事件
+  opts = {
+    enabled = true, -- 默认开启
+    trigger_events = {
+      immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- 光标离开缓冲区或窗口失焦时立即保存
+      defer_save = { "InsertLeave" }, -- 文本更改或退出插入模式后延迟保存
+      cancel_deferred_save = { "InsertEnter" }, -- 进入插入模式时取消延迟保存
+    },
+    debounce_delay = 1000, -- 延迟保存的等待时间（毫秒）
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+      -- 排除某些文件类型，例如不希望自动保存git提交信息
+      if utils.not_in(fn.getbufvar(buf, "&filetype"), { "gitcommit", "TelescopePrompt" }) then
+        return true -- 启用自动保存
+      end
+      return false -- 禁用自动保存
+    end,
+  },
+}
