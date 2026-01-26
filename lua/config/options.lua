be6a1d2de67f0733b2692a27c5639edec3bbd3ea
刘天增 <tianzeng_liu@loongjoy.com>
2026-01-26 19:45:31 +0800

    feat: add keymap for bufferline, modify the undo parameters

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://gith, "QuitPre", "VimSuspend"ub.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 1000
+vim.opt.history = 1000
