b3054de31ab29415aa7b1ecd33e3be2fddd42b39
刘天增 <tianzeng_liu@loongjoy.com>
2026-06-07 00:20:44 +0800

    feat: update

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+# installation
+
+git clone <git@github.com>:jasontz/lazyvim-template.git  ~/.config/nvim
+git clone <https://github.com/jasontz/lazyvim-template.git>  ~/.config/nvim
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,4 +10,23 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.formatoptions = vim.opt.formatoptions - { "o", "r" }
 
-vim.opt.clipboard = ""
+vim.o.clipboard = ""
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg('"'), "\n"),
+    vim.fn.getregtype('"'),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
