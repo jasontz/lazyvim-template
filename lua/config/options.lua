b564e2c328fdf8d568cd3fac97337dda4ae50180
刘天增 <tianzeng_liu@loongjoy.com>
2026-06-07 16:36:25 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,15 @@
 -- Default options that are always set: https://gith, "QuitPre", "VimSuspend"ub.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.undolevels = 1000
-vim.opt.undoreload = 1000
-vim.opt.history = 1000
+vim.opt.undolevels = 5000
+vim.opt.undoreload = 5000
+vim.opt.history = 5000
 
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.formatoptions = vim.opt.formatoptions - { "o", "r" }
+
+vim.opt.mouse = "r"
 
 vim.o.clipboard = ""
 
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,17 @@
+return {
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      terminal = {
+        -- your terminal configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        win = {
+          position = "float",
+        },
+      },
+    },
+  },
+}
